a1a60f9b1472c16c23fada7fb087cd24c8f9f858
tmgmgmg <tmgmgmg.01414@gmail.com>
2021-09-12 22:51:42 +0900

    create class components and functional component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import React from "react";
 //   );
 // }
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={() => { console.log("I am clicked") }} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => { console.log("I am clicked") }} />
-    </React.Fragment>
-  );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 // function App() {
